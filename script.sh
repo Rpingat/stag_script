df09b7cee900ed7b8198fd8a3caf5932839b3c02
Rpingat <pingatravi@gmail.com>
2020-01-20 09:39:51 +0100

    Testing stuff
    *For now set a single device value

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@
 export TERM=xterm
 
 ROOT_PATH=$PWD
+DEVICE=lettuce
 BUILD_PATH="$ROOT_PATH/out/target/product/$DEVICE"
 
 #Get Filesize
@@ -22,5 +23,5 @@ fileid=$(gdrive upload ${STAG} -p 1bYCLRWIPfuH7Uf7WT9lk7Vgj9iYhQJll | tail -1 | 
 echo "Sending Notifications to group"
 telegram-send --image "$ROOT_PATH/stag.jpg"
 telegram-send --format markdown "Device :- $DEVICE
-Download :- [SourceForge](https://sourceforge.net/projects/stagos-10/files/$DEVICE/$STAG/download) | [GDrive Mirror](https://drive.google.com/uc?id=$fileid&export=download)
+Download :- [SourceForge](https://sourceforge.net/projects/team-xtreme/file/$STAG/download) | [GDrive Mirror](https://drive.google.com/uc?id=$fileid&export=download)
 FileSize :- $size"
